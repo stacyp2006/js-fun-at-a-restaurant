e1e08723e252cb2cf39526f11b15d5d89b581e96
Stacy Potten <stacylynnaz@gmail.com>
2020-07-05 15:43:27 -0600

    Complete Order Functions

--- src/order.js
+++ src/order.js
@@ -1,7 +1,32 @@
 
+
+function takeOrder(orders, deliveryOrders) {
+  if (deliveryOrders.length <= 2) {
+    deliveryOrders.push(orders);
+  };
+}
+
+function refundOrder(orders, deliveryOrders) {
+  deliveryOrders.shift(deliveryOrders.orderNumber);
+}
+
+function listItems(deliveryOrders) {
+  return `${deliveryOrders[0].item}, ${deliveryOrders[1].item}, ${deliveryOrders[2].item}`
+}
+
+function searchOrder(deliveryOrders, item) {
+  for (var i = 0; i <= deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item.includes(item)) {
+      return true;
+    } else {
+      return false;
+    };
+  };
+}
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
