5a7282ea39a4e2945d2069c5956de7e75aeab52e
Stacy Potten <stacylynnaz@gmail.com>
2020-07-05 15:44:19 -0600

    Complete Restaurant Functions

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,40 @@
+function createRestaurant(name) {
+    var obj1 = {
+      name: name,
+      menus: {
+        breakfast: [],
+        lunch: [],
+        dinner: [],
+      }
+    };
+    return obj1;
+}
+
+function addMenuItem(restaurant, menuItem) {
+  var menuType = menuItem.type;
+  var menuArray = restaurant.menus[menuType];
+  if (menuArray.includes(menuItem) == false) {
+    menuArray.push(menuItem);
+  };
+}
+
+function removeMenuItem(restaurant, menuItem, menuType) {
+  if (menuType === "breakfast") {
+    restaurant.menus.breakfast.pop();
+    return `No one is eating our ${menuItem} - it has been removed from the ${menuType} menu!`;
+  }else if (menuType === "dinner") {
+    restaurant.menus.dinner.pop();
+    return `No one is eating our ${menuItem} - it has been removed from the ${menuType} menu!`;
+  }else if (menuItem.includes(menuItem) == true) {
+    return `Sorry, we don't sell ${menuItem}, try adding a new recipe!`;
+  }else {
+
+  }
+};
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
